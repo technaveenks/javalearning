03f46b525cef1578d0fb3eb521660e2619df99b3
technaveenks <tech.naveenks@gmail.com>
2022-10-18 16:49:30 +0530

    Initial Commit

---
+++ src/main/java/com/learning/automation/javalearning/App.java
@@ -0,0 +1,13 @@
+package com.learning.automation.javalearning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
