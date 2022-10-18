c5220e3961b197716efc640eabf08dc1c9f7725b
technaveenks <tech.naveenks@gmail.com>
2022-10-18 17:12:53 +0530

    Team members package

---
+++ src/main/java/com/learning/automation/javalearning/ujwala/FirstProgram.java
@@ -0,0 +1,34 @@
+package com.learning.automation.javalearning.ujwala;
+
+public class FirstProgram {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Naveen \" Top Gun Maverick \" ");
+		System.out.print(" Top Gun Maverick ");
+		
+		//Data type
+		int Avalue =  214748367;
+		int Bvalue =  214748367;
+		int cValue = Avalue+Bvalue;
+		System.out.println(cValue);
+		
+		//Addition  + 
+		int firstValue = 10;
+		int secondValue = 20;
+		int result = firstValue % secondValue;
+		System.out.println("Modulo Value is "+ result);
+		
+/*		
+		  Modulo value of 10 & 20 is = 10
+		  
+		  Write a program using arithmetic operations and print all the five operation
+		  values and display the result in the below format
+		  
+		  "Operator value of operand & operand is = result"
+		 
+	*/	
+		
+		
+	}
+
+}
