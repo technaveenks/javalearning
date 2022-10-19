c13f02c3fee3f0f4680c7d8516ec63b31a1f67d3
technaveenks <tech.naveenks@gmail.com>
2022-10-19 16:14:03 +0530

    Assignment-1019-ArithmeticOperation

---
+++ src/main/java/com/learning/automation/javalearning/ArithmeticOperation.java
@@ -0,0 +1,10 @@
+package com.learning.automation.javalearning;
+
+public class ArithmeticOperation {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(" Arithmetic Operation ");
+	}
+
+}
