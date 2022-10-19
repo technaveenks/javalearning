5af56a1ea3a0dd32c83ea29ef420cbb99d802f39
sarithakorrapati <115467994+sarithakorrapati@users.noreply.github.com>
2022-10-19 16:29:24 -0500

    Update Arithmeticoperations.java 10/19

--- src/main/java/com/learning/automation/javalearning/saritha/ArithmeticOperations.java
+++ src/main/java/com/learning/automation/javalearning/saritha/ArithmeticOperations.java
@@ -1,10 +1,28 @@
 package com.learning.automation.javalearning.saritha;
 
+import java.util.Scanner;
+
 public class ArithmeticOperations {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Arithmetic operations");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the first value : ");
+		int firstValue = scanner.nextInt();
+		System.out.println(firstValue);
+		System.out.println("Enter the second value : ");
+		int secondValue = scanner.nextInt();
+		System.out.println(secondValue);
+		int sum = firstValue+secondValue;
+		System.out.println("The sum of two values is: "+sum);
+		int sub = firstValue-secondValue;
+		System.out.println("The subtraction of two values is: "+sub);
+		int mul = firstValue*secondValue;
+		System.out.println("The multiplication of two values is: "+mul);
+		int div = firstValue/secondValue;
+		System.out.println("The division of two values is: "+div);
+		int mod = firstValue%secondValue;
+		System.out.println("The modulo of two values is: "+mod);
+	
 	}
 
 }
