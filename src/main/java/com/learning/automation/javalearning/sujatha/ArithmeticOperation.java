9605cc8355902752ada4b24389608cfa1306069b
sujatha1215 <115469791+sujatha1215@users.noreply.github.com>
2022-10-19 07:04:26 -0400

    ArithmeticOperation

--- src/main/java/com/learning/automation/javalearning/sujatha/ArithmeticOperation.java
+++ src/main/java/com/learning/automation/javalearning/sujatha/ArithmeticOperation.java
@@ -4,12 +4,8 @@ public class ArithmeticOperation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int a=10;
-int b=20;
-int c;
-int d=b-a;
-int e=a*b;
-System.out.println("hai");
+
+System.out.println("Arithmetic Operation");
 
 
 	}
