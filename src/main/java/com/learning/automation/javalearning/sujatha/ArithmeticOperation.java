75e353b9183de46159d98d979e92fad964527046
sujatha1215 <115469791+sujatha1215@users.noreply.github.com>
2022-10-19 06:57:57 -0400

    Assignment-1019-ArithmeticOperation

---
+++ src/main/java/com/learning/automation/javalearning/sujatha/ArithmeticOperation.java
@@ -0,0 +1,17 @@
+package com.learning.automation.javalearning.sujatha;
+
+public class ArithmeticOperation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10;
+int b=20;
+int c;
+int d=b-a;
+int e=a*b;
+System.out.println("hai");
+
+
+	}
+
+}
