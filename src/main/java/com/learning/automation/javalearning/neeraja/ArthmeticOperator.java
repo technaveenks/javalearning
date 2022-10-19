c1195fb2f86fcd9fa23509b07b13fecd1e1135c3
neeraja <nerys123@gmail.com>
2022-10-19 07:46:31 -0400

    Assignment-1019-Arthimetic operations

---
+++ src/main/java/com/learning/automation/javalearning/neeraja/ArthmeticOperator.java
@@ -0,0 +1,11 @@
+package com.learning.automation.javalearning.neeraja;
+
+public class ArthmeticOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My first program");
+
+	}
+
+}
