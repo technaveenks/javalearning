94a68986182b63884c43cd9b6156d65c99612dbd
malathip522 <malathip522@gmail.com>
2022-10-19 07:49:01 -0400

    Update ArithmeticOperation.java

--- src/main/java/com/learning/automation/javalearning/malathi/ArithmeticOperation.java
+++ src/main/java/com/learning/automation/javalearning/malathi/ArithmeticOperation.java
@@ -1,5 +1,8 @@
 package com.learning.automation.javalearning.malathi;
 
 public class ArithmeticOperation {
+	public static void main(String[] args) {
+		System.out.println("Arithmetic operation");
+	}
 
 }
