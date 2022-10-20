f3c0ec0db2cf3990c51fe744a372895813a9b904
neeraja <nerys123@gmail.com>
2022-10-19 20:15:58 -0400

    Assignment 10/19-Operator completed

--- src/main/java/com/learning/automation/javalearning/neeraja/ArthimeticOperator.java
+++ src/main/java/com/learning/automation/javalearning/neeraja/ArthimeticOperator.java
@@ -1,10 +1,32 @@
 package com.learning.automation.javalearning.neeraja;
 
+import java.util.Scanner;
+
 public class ArthimeticOperator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+         Scanner sc = new Scanner(System.in);
+		
+		System.out.println(" Enter the first Value :");
+		int firstValue = sc.nextInt();
+		
+		
+		System.out.println(" Enter the Second Value :");
+		int secondValue = sc.nextInt();
+		
+		int sum = firstValue+ secondValue;
+		System.out.println(" The sum of two values are "+sum);
+		
 
+		int sub = firstValue- secondValue;
+		System.out.println(" The subtraction of two values are "+sub);
+		
+		int mul = firstValue* secondValue;
+		System.out.println(" The multiplication of two values are "+mul);
+		
+		int div = firstValue/ secondValue;
+		System.out.println(" The division of two values are "+div);
 	}
 
 }
