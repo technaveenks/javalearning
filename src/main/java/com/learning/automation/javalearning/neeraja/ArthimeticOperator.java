971c7bed682ee26629978820c54e8399aeedf127
neeraja <nerys123@gmail.com>
2022-10-19 20:01:47 -0400

    Assignment-10/19

---
+++ src/main/java/com/learning/automation/javalearning/neeraja/ArthimeticOperator.java
@@ -0,0 +1,10 @@
+package com.learning.automation.javalearning.neeraja;
+
+public class ArthimeticOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
