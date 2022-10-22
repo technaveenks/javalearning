4b349ce8de4ac843e11b5350d8729f123d446a6e
sujatha1215 <115469791+sujatha1215@users.noreply.github.com>
2022-10-22 09:27:07 -0400

    UnderstandingMethods

---
+++ src/main/java/com/learning/automation/javalearning/sujatha/UnderstandingMethods.java
@@ -0,0 +1,55 @@
+package com.learning.automation.javalearning.sujatha;
+
+public class UnderstandingMethods {
+
+	int FirstValue=50;
+	int SecondValue=10;
+	
+	public void Addtion()
+	{
+		int Result=FirstValue+SecondValue;
+		System.out.println("Addition of Two Values "+ Result);
+	}
+	
+	public void Subtraction()
+	{
+		int Result=FirstValue-SecondValue;
+		System.out.println("Subtraction of Two Values "+ Result);
+	}
+	
+	public void Multiplication(int a,int b)
+	{
+		int r=a*b;
+		System.out.println(r);
+		
+		//int Result=FirstValue*SecondValue;
+		//System.out.println("Multiplication of Two Values "+ Result);
+	}
+	
+	
+	public void Division()
+	{
+		int Result=FirstValue/SecondValue;
+		System.out.println("Division of Two Values "+ Result);
+	}
+	
+	public void Modulo()
+	{
+		int Result=FirstValue%SecondValue;
+		System.out.println("Modulo of Two Values "+ Result);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		UnderstandingMethods understandingMethods=new UnderstandingMethods();
+		System.out.println("Understanding Of Methods"+"\n");
+		
+		understandingMethods.Addtion();
+		understandingMethods.Subtraction();
+		understandingMethods.Multiplication(2,3);
+		understandingMethods.Division();
+		understandingMethods.Modulo();
+		
+		}
+
+}
