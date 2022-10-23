4f39b95ca5134713528bb29cdd5e587910fe92ad
technaveenks <tech.naveenks@gmail.com>
2022-10-23 18:40:45 +0530

    learning if, methods with return type and constructors

---
+++ src/main/java/com/learning/automation/javalearning/UnderstandingLoops.java
@@ -0,0 +1,16 @@
+package com.learning.automation.javalearning;
+
+public class UnderstandingLoops {
+	
+	public static void main(String[] args) {
+		int fValue = 20,sValue=25, tValue=33;
+		if(fValue>sValue) {
+			System.out.println("First Value is greater");
+		}else if(sValue > tValue){
+			System.out.println(" Second Value is greater");
+		}else {
+			System.out.println(" Third Value is greater ");
+		}
+	}
+
+}
