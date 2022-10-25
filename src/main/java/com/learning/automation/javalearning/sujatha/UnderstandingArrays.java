0c0167375768bde760c14fdb7e51eab565beb6d7
sujatha1215 <115469791+sujatha1215@users.noreply.github.com>
2022-10-25 08:09:31 -0400

    Create UnderstandingArrays.java

---
+++ src/main/java/com/learning/automation/javalearning/sujatha/UnderstandingArrays.java
@@ -0,0 +1,45 @@
+package com.learning.automation.javalearning.sujatha;
+
+public class UnderstandingArrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int []a= {2,3,1,5,9,6};
+		System.out.println(a.length);
+		System.out.println(a[4]);
+		//Displaying Array Data-
+		for(int b:a)
+		{
+			System.out.println("*"+b);
+		}
+		//Array with String Datatype
+		String s[]= {"surya","Karthik","Vinodh","Raja","Ram"};
+				for(String s1:s) {
+					System.out.println(s1);
+		}
+		
+		//Multidimensional array
+		int x[][]= {{2,3,4,5},{9,8,7,6},{4,6,7,8}};
+		System.out.println("**"+x[2][2]);
+		for(int [] y:x)
+		{
+			for(int z:y)
+			{
+				System.out.print(z);
+			}
+		}
+		
+			//Multidimensionsal String Array
+		String stringarray[][]= {{"uma","radhi","radha","sam"},{"soni","girija","uma2","nayan"},{"happy","fara","sara","tara"}};
+
+		for(String []sarray:stringarray)
+		{
+			for(String sarrayvalues:sarray)
+			{
+				System.out.println("!!"+sarrayvalues);
+			}
+		}
+	}
+
+}
