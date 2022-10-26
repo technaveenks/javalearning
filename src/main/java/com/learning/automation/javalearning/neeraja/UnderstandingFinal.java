61b2fe1f764faef60fde5be2d497bceb3e0f1193
neeraja <nerys123@gmail.com>
2022-10-26 13:59:30 -0400

    commit static final and casting

---
+++ src/main/java/com/learning/automation/javalearning/neeraja/UnderstandingFinal.java
@@ -0,0 +1,20 @@
+package com.learning.automation.javalearning.neeraja;
+
+public class UnderstandingFinal {
+
+	
+	final static int age=25;
+	
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Assigning new value to final variable
+		//age=age+10;
+		//Calling static final variable under main meyhod
+		System.out.println(age);
+
+	}
+
+}
