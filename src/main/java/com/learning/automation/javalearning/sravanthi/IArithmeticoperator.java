3325cd2784ffa4daed722139cb0a3069d95e5a52
sravanthims <115467919+sravanthims@users.noreply.github.com>
2022-10-27 07:38:03 -0400

    UnderstandingInheritance

---
+++ src/main/java/com/learning/automation/javalearning/sravanthi/IArithmeticoperator.java
@@ -0,0 +1,12 @@
+package com.learning.automation.javalearning.sravanthi;
+
+public interface IArithmeticoperator {
+	
+	public void addition(int a , int b);
+	public void subtraction(int a,int b);
+	public void mutiplication(int a,int b);
+	public void division(int a,int b);
+	public void modulo(int a,int b);
+
+
+}
