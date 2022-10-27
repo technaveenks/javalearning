f7b00d34c3094ba1b4ebb7657d4f41df95c429cd
gayatri-gopavaram <115467986+gayatri-gopavaram@users.noreply.github.com>
2022-10-27 07:50:55 -0400

    10/27 ---Interfaces
    code using interfaces for abstraction

---
+++ src/main/java/com/learning/automation/javalearning/gayathri/MathematicalOperations.java
@@ -0,0 +1,52 @@
+package com.learning.automation.javalearning.gayathri;
+
+public class MathematicalOperations implements iArthmeticOperators{
+
+	public static void main(String[] args) {
+		MathematicalOperations mo = new MathematicalOperations();
+		mo.addition(10,5);
+		mo.subtraction(10,5);
+		mo.multiplication(10,5);
+		mo.division(10,5);
+		mo.modulo(10,5);
+		
+	}
+
+	@Override
+	public void addition(int a, int b) {
+		int c=a+b;
+		System.out.println(c);
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void subtraction(int a, int b) {
+		// TODO Auto-generated method stub
+		int c=a-b;
+		System.out.println(c);
+	}
+
+	@Override
+	public void multiplication(int a, int b) {
+		// TODO Auto-generated method stu
+		int c=a*b;
+		System.out.println(c);
+	}
+
+	@Override
+	public void division(int a, int b) {
+		// TODO Auto-generated method stub
+		int c=a/b;
+		System.out.println(c);
+	}
+
+	@Override
+	public void modulo(int a, int b) {
+		// TODO Auto-generated method stub
+		int c=a%b;
+		System.out.println(c);
+		
+		
+	}
+}
---
+++ src/main/java/com/learning/automation/javalearning/gayathri/iArthmeticOperators.java
@@ -0,0 +1,11 @@
+package com.learning.automation.javalearning.gayathri;
+
+public interface iArthmeticOperators {
+	
+	public void addition(int a,int b);
+	public void subtraction(int a,int b);
+	public void multiplication(int a,int b);
+	public void division(int a,int b);
+	public void modulo(int a,int b);
+
+}
