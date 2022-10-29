bab50e017fbd7b43fa0f62e7f56c09c0af7a6990
sonu <43224767+Sonu97@users.noreply.github.com>
2022-10-29 01:41:28 -0400

    added Restaurant and Food App.

---
+++ src/main/java/com/learning/automation/javalearning/sonu/RestaurantApp.java
@@ -0,0 +1,40 @@
+package com.learning.automation.javalearning.sonu;
+
+public class RestaurantApp {
+    private String name;
+    private int phoneNumber;
+    private String address;
+    private String menu;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getMenu() {
+        return menu;
+    }
+
+    public void setMenu(String menu) {
+        this.menu = menu;
+    }
+}
