3ba8b1e410482e2925b260314fb541888d3dd65a
sujatha1215 <115469791+sujatha1215@users.noreply.github.com>
2022-10-30 22:12:09 -0400

    Inheritance

---
+++ src/main/java/com/learning/automation/javalearning/sujatha/Inheritance/ClassZ.java
@@ -0,0 +1,9 @@
+package com.learning.automation.javalearning.sujatha.Inheritance;
+
+public class ClassZ {
+
+	public void MethodZ()
+	{
+		System.out.println("This method is from ClassZ");
+	}
+}
---
+++ src/main/java/com/learning/automation/javalearning/sujatha/Inheritance/GrandParentA.java
@@ -0,0 +1,9 @@
+package com.learning.automation.javalearning.sujatha.Inheritance;
+
+public class GrandParentA extends ClassZ {
+
+	public void MethodA()
+	{
+		System.out.println("This method is from GrandParent");
+	}
+}
