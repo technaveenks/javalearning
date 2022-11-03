e8c900f8bda2cb7fbc254480c6123c5bb0db3314
gayatri-gopavaram <115467986+gayatri-gopavaram@users.noreply.github.com>
2022-11-02 23:21:00 -0400

    Update iArthmeticOperators.java
    interface

--- src/main/java/com/learning/automation/javalearning/gayathri/iArthmeticOperators.java
+++ src/main/java/com/learning/automation/javalearning/gayathri/iArthmeticOperators.java
@@ -2,6 +2,7 @@ package com.learning.automation.javalearning.gayathri;
 
 public interface IArthmeticOperators {
 	//updated the interface name
+	//added new interface as IArthmeticOperators
 	public void addition(int a,int b);
 	public void subtraction(int a,int b);
 	public void multiplication(int a,int b);
