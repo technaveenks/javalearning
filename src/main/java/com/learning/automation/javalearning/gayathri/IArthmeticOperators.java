5d57e4bb618ab39047ba9bbf75cb5884e1dd89e4
gayatri-gopavaram <115467986+gayatri-gopavaram@users.noreply.github.com>
2022-11-02 23:30:16 -0400

    changed the interface name
    Interface

--- src/main/java/com/learning/automation/javalearning/gayathri/IArthmeticOperators.java
+++ src/main/java/com/learning/automation/javalearning/gayathri/IArthmeticOperators.java
@@ -3,10 +3,12 @@ package com.learning.automation.javalearning.gayathri;
 public interface IArthmeticOperators {
 	//updated the interface name
 	//added new interface as IArthmeticOperators
+	//changed the interface name
 	public void addition(int a,int b);
 	public void subtraction(int a,int b);
 	public void multiplication(int a,int b);
 	public void division(int a,int b);
 	public void modulo(int a,int b);
+	
 
 }
