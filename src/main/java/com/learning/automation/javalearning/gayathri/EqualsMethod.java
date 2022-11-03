7aa414541a0e5f409757eefd9fc7017e3b62d291
gayatri-gopavaram <115467986+gayatri-gopavaram@users.noreply.github.com>
2022-11-02 22:40:12 -0400

    10/29 ---EqualsMethod
    code using EqualsMethod

---
+++ src/main/java/com/learning/automation/javalearning/gayathri/EqualsMethod.java
@@ -0,0 +1,28 @@
+package com.learning.automation.javalearning.gayathri;
+
+public class EqualsMethod {
+	
+	public static void main(String[] args) {
+		String s1 = "Automation";
+		String s2 = "automation";
+		boolean isequals = s2.equals(s1);
+		System.out.println(isequals);
+		//Checking the condition using if statement
+		if(s1==s2) {
+			System.out.println("Both are same");
+		}else
+		{
+			System.out.println("Both are different");
+		}
+		
+	//	int a=10;
+	//	System.out.println(a);
+		
+		}
+
+	
+
+	}
+	
+
+
