5ff9a7c28f62981c3666fa9f7b42714ddffd4f00
bjhansi2001 <115467604+bjhansi2001@users.noreply.github.com>
2022-11-03 22:45:40 -0400

    Create FileOperation.java

---
+++ src/main/java/com/learning/automation/javalearning/jhansi/FileOperation.java
@@ -0,0 +1,36 @@
+package com.learning.automation.javalearning.jhansi;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FileOperation {
+
+	public static void main(String[] args) throws IOException {
+
+			File file = new File("C:\\SampleFile\\SampleFile.txt");
+			if (file.createNewFile()) {
+				System.out.println("File Created" + file.getName() + " "+ file.getAbsolutePath());
+			}else {
+				System.out.println("File already exist");
+			}
+			
+			Scanner scan = new Scanner(file);
+			while (scan.hasNextLine()) {
+				System.out.println("File contents are" + scan.nextLine());
+			}
+			scan.close();
+			
+			FileWriter writer = new FileWriter(file);
+			writer.append("Text from console to the existing text");
+			writer.close();
+			scan = new Scanner(file);
+			while (scan.hasNextLine()) {
+				System.out.println(scan.nextLine());
+				
+			}
+			scan.close();
+	}
+
+}
